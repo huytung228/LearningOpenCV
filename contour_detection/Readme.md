5e359a6a26e82a67a17edf50c98228cfdb085c40
tung.le <tung.le@seta-international.vn>
2021-03-31 18:16:15 +0700

    contour

---
+++ contour_detection/Readme.md
@@ -0,0 +1,2 @@
+- Link contour feature:
+https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_contours/py_contour_features/py_contour_features.html
--- contour_detection/contour_dectection.py
+++ contour_detection/contour_dectection.py
@@ -1,21 +1,45 @@
 import cv2
-img = cv2.imread('bird.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+import numpy as np
 
-def resize_image(img, scale = 0.3):
-    height, width = img.shape[:2]
-    height = int(height * scale)
-    width = int(width * scale)
-    return cv2.resize(img, (width, height))
+img = cv2.imread('shape.png')
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-img = resize_image(img)
-cv2.imshow('bird', img)
+def empty(a):
+    pass
 
-img_blur =  cv2.GaussianBlur(img, (3,3), 0)
-canny = cv2.Canny(img, 125, 175)
-contours, hierarchies = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-print(len(contours))
-# cv2.imshow('canny', canny)
-# cv2.imshow('blur', img_blur)
+cv2.namedWindow("binary")
+trackbar = cv2.createTrackbar("min threadsold", "binary", 70, 255, empty)
+trackbar = cv2.createTrackbar("max threadsold", "binary", 255, 255, empty)
 
-cv2.waitKey(0)
+# def resize_image(img, scale = 0.3):
+#     height, width = img.shape[:2]
+#     height = int(height * scale)
+#     width = int(width * scale)
+#     return cv2.resize(img, (width, height))
+
+# img = resize_image(img, 1)
+
+min_th = cv2.getTrackbarPos("min threadsold", "binary")
+max_th = cv2.getTrackbarPos("max threadsold", "binary")
+
+_, imgBinary = cv2.threshold(imgGray,min_th,max_th,cv2.THRESH_BINARY)
+
+img_blur =  cv2.GaussianBlur(imgBinary, (3,3), 0)
+
+contours, hierarchies = cv2.findContours(img_blur, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+cv2.drawContours(img, contours, -1, (0, 255, 0), 5)
+
+x,y,w,h = cv2.boundingRect(contours[0])
+img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+rect = cv2.minAreaRect(contours[0])
+box = cv2.boxPoints(rect)
+box = np.int0(box)
+img = cv2.drawContours(img,[box],0,(0,0,255),2)
+
+
+cv2.imshow('binary', img)
+
+if cv2.waitKey(0) & 0xFF == ord('q'):
+   exit()
